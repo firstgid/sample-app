36f56edcb4125939a072176d66280774b4e7fba4
mackruk <mwilczek@mail.ru>
2015-12-16 22:39:46 +0000

    sample-app

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,20 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # Preview this email at http://https://rails-tutorial-lapudavo.c9.io/rails/mailers/user_mailer/account_activation
+  # https://rails-tutorial-lapudavo.c9.io/
+  def account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
+  end
+
+  # Preview this email at https://rails-tutorial-lapudavo.c9.io/rails/mailers/user_mailer/password_reset
+  def password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
+  end
+
+end
